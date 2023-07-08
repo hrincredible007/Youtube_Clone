1de2ed1e8a968c30e068b0493135b0151a51dc10
hrincredible007 <hritikbhardwaj2002@gmail.com>
2023-07-09 02:36:16 +0530

    Initial Setup Done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import { ReactDOM } from 'react';
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Hello world!</h1>
+        <h2 className="text-2xl">Hritik Bhardwaj</h2>
+    </div>
+  );
+};
+export default App;
